ec1d9011b38a837e63f11695adcf5127c18f46e3
ketongwang <kentwang16@gmail.com>
2014-08-28 16:43:42 -0500

    Dr. Porter's code for cleansing of raw accident data

--- Rcode/data_clean.R
+++ Rcode/data_clean.R
@@ -5,6 +5,35 @@
 #  Author: Ketong Wang
 ##########################################################
 
+#-- Read in the data
 a <- read.csv("data/accident.csv")
-classInfo <- read.csv("data/classInfo.csv")
+vClass <- read.csv("data/classInfo.csv")
 coordinate <- read.csv("data/coordinate.csv")
+a[(a == "<Null>")] = NA  # convert <Null> to NA
+
+#-- convert categorical data to "factor"
+ind = which(vClass$Class=="categorical")
+for(j in ind) a[,j] = factor(a[,j])
+
+#-- convert numeric data to "numeric
+ind = which(vClass$Class=="numeric")
+for(j in ind) class(a[,j]) = "numeric"
+
+#-- replace missing AADT
+missing.AADT = is.na(a$AADT)   # indicates if AADT is missing (NA)
+a$AADT[missing.AADT] = a$IntAADT[missing.AADT]
+
+#-- Create intersection related variables other intersection 
+#   variables to consider: variation in AADT at intersection, 
+#   total volume at intersection, etc
+
+b = strsplit(as.character(a$LegID),"_")
+b = do.call(rbind,b)
+IntID = factor(b[,1])    # intersection ID variable
+segTab = as.data.frame(table(IntID))
+ind = match(IntID,segTab$IntID)
+numSegs = as.numeric(segTab$Freq[ind])
+total = tapply(a$AADT,IntID,sum)  
+totalAADT = as.numeric(total[ind]) # total traffic at intersection
+
+a = data.frame(IntID,numSegs,a,totalAADT)  # add new variables
