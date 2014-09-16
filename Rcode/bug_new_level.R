d58a17b5578748c3a5da2be8c002fae2009593b1
ketongwang <kentwang16@gmail.com>
2014-09-16 15:17:42 -0500

    Use function glm.my(). Ran through with some warnings. "In predict.lm(object, newdata, se.fit, scale = 1, type = ifelse(type ==  :   prediction from a rank-deficient fit may be misleading"

--- Rcode/bug_new_level.R
+++ Rcode/bug_new_level.R
@@ -1,4 +1,8 @@
-X = model.matrix(fmla,data, drop.unused.levels = T)
+data <- a
+fold <- cvfolds(dim(data)[1])
+show.pb <- TRUE
+
+X = model.matrix(fmla,data)
 Y = as.matrix(data[,as.character(fmla[[2]])])
 offset = model.offset(model.frame(fmla,data))  
 
@@ -11,8 +15,8 @@ if(show.pb) pb = txtProgressBar(style=3,min=min(K),max=max(K))
 for(k in K) {
   test = which(fold == k)
   train = which(fold != k)
-
-  fit0 = glm(fmla,data=data[train,],family=poisson)
+  
+  fit0 = glm.my(fmla,data=data[train,],family=poisson)
   fit = step(fit0, trace = 0)
   mu[test] = predict(fit,newdata=data[test,],type="response")
   if(show.pb) setTxtProgressBar(pb,k)
@@ -20,3 +24,13 @@ for(k in K) {
 
 Xy <- data.frame(as.matrix.data.frame(X), y=Y)
 bglm <- bestglm(Xy[, -1])
+
+#-------------------------------------------------
+#- Diagnostics: Error - Factor has new levels
+#  - records of '5' have been removed. And '4'?
+#-------------------------------------------------
+levels(a[, "LegTCType"])
+which(a[, "LegTCType"] == '5') # there is only one level 5
+
+unique(a[train, "LegTCType"]) #[1] 2 1 4
+unique(a[test, "LegTCType"]) #[1] 1 2 5
