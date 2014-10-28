2921995f6047bcba2844dde4ecc9b3311734c17f
ketongwang <kentwang16@gmail.com>
2014-10-27 21:16:54 -0500

    n.trees = 10000, interaction.depth = 3 changes the relative importance results.

--- Rcode/var_selection_offset.R
+++ Rcode/var_selection_offset.R
@@ -12,8 +12,8 @@ poisReg.offset <- step(glm(fmla.offset, data = a, family = "poisson"))
 negBino.offset <- step(glm.nb(fmla.offset, data = a))
 
 # glmnet
-glmnetPois.cv.offset <- cv.glmnet(model.matrix(fmla.offset, data = a)[,-1], as.matrix(a[, "X5YrCrashCount"]), 
-                                  family = "poisson", offset = log(a$Traffic))
+glmnetPois.cv.offset <- cv.glmnet(model.matrix(fmla.offset, data = a)[,-1], 
+                                  as.matrix(a[, "X5YrCrashCount"]), family = "poisson", offset = log(a$Traffic))
 beta.offset <- as.matrix(coef(glmnetPois.cv.offset, s="lambda.1se"))
 beta.offset.names <- rownames(beta.offset)
 beta.offset <- as.numeric(beta.offset)
@@ -23,6 +23,7 @@ names(beta.offset) <- beta.offset.names
 treePois.offset <- rpart(fmla.offset, data = a, method = "poisson")
 
 # gbm boosting trees
-gbmPois.offset <- gbm(fmla.offset, data = a, distribution = "poisson")
-
+gbmPois.offset <- gbm(fmla.offset, data = a, distribution = "poisson", 
+                      n.trees = 10000, interaction.depth = 3)
+summary_gbm <- summary(gbmPois.offset)
 
